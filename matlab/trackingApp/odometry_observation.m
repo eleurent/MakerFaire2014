74e1813d0257079ddb7d4b9c8f4ab7b0c1d68ed0
Edouard Leurent <eleurent@gmail.com>
2014-05-30 21:20:55 +0200

    Fixed bearing observation not being wrapped to pi

--- matlab/trackingApp/odometry_observation.m
+++ matlab/trackingApp/odometry_observation.m
@@ -1,5 +1,8 @@
 function Y = odometry_observation(x,s)
   bearing = atan2(s(2) - x(2),s(1) - x(1)) - x(3);
+  %wrapToPi
+  num2pi = floor(bearing/(2*pi) + 0.5);
+  bearing = bearing - num2pi*2*pi;
   distance = norm(s-x(1:2));
   Y = [bearing;distance];
 end
