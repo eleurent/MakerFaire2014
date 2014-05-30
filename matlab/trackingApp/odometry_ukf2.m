c3e74e62183cbd9ac0f86f2f1f1b7a7cbf307d1b
Edouard Leurent <eleurent@gmail.com>
2014-05-30 20:22:42 +0200

    Took camera delay into account in EKF update + updated todo list

--- matlab/trackingApp/odometry_ukf2.m
+++ matlab/trackingApp/odometry_ukf2.m
@@ -1,28 +1,38 @@
-function M_out = odometry_ukf2(el,er,WheelRadius,AxleLength,EncRes,Targets,DistanceCameraTargets,BearingCameraTargets)
-persistent P M A Q R bruit_odometrie bruit_bearing bruit_distance f_param
+% MakerFaire Paris 2014
+% Martin de Gourcuff, Alexandre Lefort, Edouard Leurent
+function M_out = odometry_ukf2(el,er,WheelRadius,AxleLength,EncRes,Targets,DistanceCameraTargets,BearingCameraTargets,N_delay)
+persistent P M A Q R bruit_odometrie bruit_bearing bruit_distance f_param M_history P_history encoders_history i_now
 
 %% Init
 if isempty(P)
-  bruit_odometrie = 2*pi/180;
-  bruit_bearing = 5*pi/180;
-  bruit_distance = 10;
+  bruit_odometrie = 2*pi/EncRes;
+  bruit_bearing = 0.05;
+  bruit_distance = 0.05;
   M = [50; 50; 0];
-  P = diag([0.001 0.001 0.001]);
-  R = diag([bruit_bearing, bruit_distance].^2);
+  P = diag([0.0001 0.0001 0.0001]);
   Q = diag((bruit_odometrie*[WheelRadius/2;WheelRadius/2;WheelRadius/AxleLength]).^2);
+  M_history = zeros(3,1,N_delay);
+  P_history = zeros(3,3,N_delay);
+  encoders_history = zeros(2,N_delay);
+  for i=1:N_delay
+      M_history(:,i) = M;
+      P_history(:,:,i) = P;
+  end
+  i_now = 1;
 end
 
-%% EKF predict
-f_param = [double(el)*2*pi/EncRes;double(er)*2*pi/EncRes;WheelRadius;AxleLength];
-A = double(odometry_jacobian(M, f_param));
-M = double(odometry_transition(M,f_param));
-P = double(A*P*A' + Q);
+% Start by storing encoders values
+encoders_history(:,i_now) = [el;er];
 
-%% EKF update
+%% EKF Update
+didUpdate = false;
+% Go back N steps in time to be in sync with measure
+i_old = mod(i_now-1+1,N_delay)+1; % +1=-(N-1) [N]
+M_old = M_history(:,i_old);
 for i=1:length(DistanceCameraTargets)
-    if(DistanceCameraTargets(i) ~= 0 && BearingCameraTargets(i)~=0)
+    if(DistanceCameraTargets(i) ~= 0 && BearingCameraTargets(i)~=0)        
         % Identify seen target
-        PositionTargetObs = M(1:2)+double(DistanceCameraTargets(i))*[cos(M(3)+double(BearingCameraTargets(i))*pi/180); sin(M(3)+double(BearingCameraTargets(i))*pi/180)];
+        PositionTargetObs = M_old(1:2)+double(DistanceCameraTargets(i))*[cos(M_old(3)+double(BearingCameraTargets(i))*pi/180); sin(M_old(3)+double(BearingCameraTargets(i))*pi/180)];
         minDist = inf;
         for j = 1:length(Targets)
             dist = norm(PositionTargetObs-Targets(j,:)');
@@ -33,17 +43,46 @@ for i=1:length(DistanceCameraTargets)
         end
         PositionTargetTrue = Targets(targetFound,:)';
         
-        if norm(PositionTargetTrue - PositionTargetObs) < 40
-        % Perform update step
-        H = odometry_observation_jacobian(M,PositionTargetTrue);
-        MU = odometry_observation(M,PositionTargetTrue);
-        Z = [double(BearingCameraTargets(i))*pi/180;double(DistanceCameraTargets(i))];
-        S = H*P*H' + R;
-        K = P*H'/S;
-        M = double(M + K*(Z-MU));
-        P = double(P - K*S*K');
+        if norm(PositionTargetTrue - PositionTargetObs) < 30
+            % Set current state to old state
+            if ~didUpdate
+                i_now = i_old;
+                M = M_old;
+                P =  P_history(:,:,i_now);
+                didUpdate = true;
+            end
+
+            % Perform update step
+            H = odometry_observation_jacobian(M,PositionTargetTrue);
+            MU = odometry_observation(M,PositionTargetTrue);
+            Z = [double(BearingCameraTargets(i))*pi/180;double(DistanceCameraTargets(i))];
+
+            % Dynamic measurement noise : std(uniform noise*x) = (max-min)/sqrt(12)*x
+            R = diag([bruit_bearing*2/sqrt(12)*abs(MU(1)), bruit_distance*2/sqrt(12)*abs(MU(2))].^2);
+            S = H*P*H' + R;
+            K = P*H'/S;
+            M = double(M + K*(Z-MU));
+            P = double(P - K*S*K');
         end
     end
+end
+
+%% EKF predict
+if didUpdate
+    nPredictions = N_delay;
+else
+    nPredictions = 1;
+end
+for i = 1:nPredictions
+    f_param = [double(encoders_history(1,i_now))*2*pi/EncRes;double(encoders_history(2,i_now))*2*pi/EncRes;WheelRadius;AxleLength];
+    A = double(odometry_jacobian(M, f_param));
+    M = double(odometry_transition(M,f_param));
+    P = double(A*P*A' + Q);
+    
+    % Write new state in history
+    i_now = mod(i_now-1+1,N_delay)+1; % +1
+    M_history(:,i_now) = M;
+    P_history(:,:,i_now) = P;
 end
 
 M_out = M;
